dfe17e9c67292153297554cb99d82569b430e75b
Rdanzz <zidhanraffly123@gmail.com>
2024-10-02 19:26:32 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,57 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Basic Routes
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+// Cara 1
+// Bisa mengirimkan argumen dan lebih dari 1 argumen
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Zidhan Raffly', 'alamat' => 'Karawang']);
+});
+
+// Cara 2
+// Route::view('/about', 'about');
+
+
+// Route Controller
+// Route Redirect
+// jadi akan dilempar ke /new-url
+Route::redirect('/old-url', '/new-url');
+
+// Route Parameter
+Route::get('/user/{id}', function ($id) {
+    // Cara 1
+    // return "User ID: ". $id;
+
+    // Cara 2
+    return "Hai, user ID kamu adalah $id";
+
+});
+
+Route::get('/about/{id}', function($id) {
+    return view ('about.detail', ['id' => $id, 'nama' => 'Zidhan Raffly']);
+});
+
+// Route Prefix
+// Kode pertama (prefix('admin')) menambahkan admin pada URL, sehingga URL hasil akhirnya adalah /admin/users.
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return 'ini adalah user dan admin';
+    });
+});
+
+// Route name prefix
+
+// Kode kedua (prefix('admin.')) menambahkan prefix ke nama rute dan menggunakan penamaan rute (->name('users')), sehingga nama rutenya menjadi admin.users, tapi URL-nya tetap /users.
+Route::name('admin.')->group(function () {
+    Route::get('/users', function () {
+        return 'ini adalah user dan admin';
+    })->name('users');
+});
+
+
